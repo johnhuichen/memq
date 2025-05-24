d6f854418c72b6a6ab8ccc055ceb82a831e729f9
John Chen <johnhuichen@gmail.com>
2025-05-24 16:22:34 -0400

    Add commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "mycli", version, about = "A CLI with subcommands")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Add a path to watchlist
+    /// Example: memq add /path/to/example.md /path/to/example
+    Add {
+        /// The path will be query for markdown files. The path can be for a file or folder.
+        #[arg(required = true)]
+        paths: Vec<String>,
+    },
+
+    /// Remove a path from watchlist
+    /// Example: memq remove /path/to/example.md /path/to/example
+    Remove {
+        /// The path will be query for markdown files. The path can be for a file or folder.
+        #[arg(required = true)]
+        paths: Vec<String>,
+    },
+
+    /// List paths in the watchlist
+    /// Example: memq list
+    List,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Add { paths } => {
+            println!("Add paths: {:?}", paths);
+        }
+        Commands::Remove { paths } => {
+            println!("Remove paths: {:?}", paths);
+        }
+        Commands::List {} => {
+            println!("List paths");
+        }
+    }
 }
